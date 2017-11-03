86cf51f65b329f233f66f03f0c15d39b3c4e4d27
Dylan Sellers <dylan.sellers@hotmail.com>
2017-11-02 23:31:32 -0600

    haptic start

---
+++ SpryCareWatchApp Extension/HapticReponse.swift
@@ -0,0 +1,23 @@
+//
+//  HapticReponse.swift
+//  SpryCareWatchApp Extension
+//
+//  Created by Dylan Sellers on 11/2/17.
+//  Copyright © 2017 Dylan Sellers. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import WatchKit
+
+class HapticResponse: InterfaceController {
+    
+    
+    
+}
+
+//current().play(WKHapticType)
+
+
+
+
