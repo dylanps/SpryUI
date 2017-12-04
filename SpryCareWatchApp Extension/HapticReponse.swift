b6a9c83b8798238e8ebd2bcfe18b9152fbe08bd3
Dylan Sellers <dylan.sellers@hotmail.com>
2017-12-04 10:37:16 -0700

    More Haptic

--- SpryCareWatchApp Extension/HapticReponse.swift
+++ SpryCareWatchApp Extension/HapticReponse.swift
@@ -11,28 +11,36 @@ import UIKit
 import WatchKit
 
 class HapticResponse: WKInterfaceController {
+    
+    @IBOutlet var myTimer: WKInterfaceLabel!
+    
+    var seconds = 60
+    var isTimerRunning = false
+    var timer = Timer()
+    var alarmValue = 100
+  /*func runTimer()
+  {
+    timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: HapticResponse.updateTimer(), userInfo: nil, repeats: true)
 
+    }
+    
+    func updateTimer() {
+        seconds -= 1     //This will decrement(count down)the seconds.
+        
+        //This will update the label.
+    }
+    */
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        
         let generator = WKInterfaceDevice()
-        
-       // while()
-       // {
-            generator.play(WKHapticType.success)
-            
-            print("Vibrate!")
-        //}
-        
+    
+        generator.play(WKHapticType.failure)
+        print("Vibrate!")
         
         // Configure interface objects here.
     }
-    
-
-    
-    //let buzz = WKHapticType.notification
-    //WKInterfaceDevice().current().play(.success)
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
